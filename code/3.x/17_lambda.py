922c334c823dbb691ab143be4d92ea879baf7aa1
hkkhuang <hkkhuang@163.com>
2018-06-13 23:26:40 +0800

    17_lambda.py

---
+++ code/3.x/17_lambda.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Script Name   : 17_lambda.py
+# Author        : huangkeke
+# Time          : 2018/6/13 21:58
+# Contact       : hkkhuang@163.com
+# Description	: lambda表达式
+
+add = lambda x, y: x + y
+print(add(4, 6))
+
+
